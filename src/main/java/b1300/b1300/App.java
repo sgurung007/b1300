c44d8a92d485d191101db62afd00e74e644fb936
surajgurung <surajgurung@192.168.0.13>
2020-06-01 21:18:46 -0500

    initial commit

---
+++ src/main/java/b1300/b1300/App.java
@@ -0,0 +1,13 @@
+package b1300.b1300;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
